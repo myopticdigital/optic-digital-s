560682941bfd41f900e5056aa00cb1d6acbd3bf5
Israel Yaya <israelyaya0@gmail.com>
2025-09-30 06:43:19 +0100

    ssh

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Route, Routes } from "react-router";
 import Layout from "./components/Layout";
 import Home from "./pages/Home/Home";
--- src/assets/svgs/svg.jsx
+++ src/assets/svgs/svg.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 export const Email = () => {
   return (
     <svg
